2a1d057ab327e97aa5c9628907f527e6eac625cb
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-19 00:27:52 +0530

    [Kumar Shubham]Added:Array returned two string functions are equal or not

--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -7,6 +7,24 @@ namespace AlgorithmPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("****Welcome to the Algorithm Programs****");
+            Console.WriteLine("============================================================");
+            Console.WriteLine("1.The Array returned two string functions are equal or not");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    String[] strOne = { "Hello", "Hi", "Bye-Bye" };
+                    String[] strTwo = { "Hello", "Hi", "Bye-Bye" };
+                    if (TwoStringFunction.CheckEquality(strOne, strTwo))
+                    {
+                        Console.WriteLine("Both strings are same");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Both string are Not same");
+                    }
+                    break;
+            }
         }
     }
 }
---
+++ AlgorithmPrograms/AlgorithmPrograms/TwoStringFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class TwoStringFunction
+    {
+        public static bool CheckEquality(String[] strOne, String[] strTwo)
+        {
+            if(strOne==strTwo)
+            {
+                return true;
+            }
+            if(strOne==null || strTwo==null)
+            {
+                return false;
+            }
+            int len = strOne.Length;
+            if (len != strTwo.Length)
+            {
+                return false;
+            }
+            for(int i=0; i<len; i++)
+            {
+                if (!strOne[i].Equals(strTwo[i])) 
+                {
+                    return false;
+                }
+            }return true;
+        }
+    }
+}
